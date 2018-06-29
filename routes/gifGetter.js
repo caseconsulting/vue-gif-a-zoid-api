e076d7cb78c7e4aa09d47b41977de41f636bfcda
Joseph Thompson <josephthompson@Josephs-MacBook-Pro.local>
2018-06-29 08:06:55 -0400

    axios-and-router-get-request: changed the axios path/routing

--- routes/gifGetter.js
+++ routes/gifGetter.js
@@ -4,24 +4,20 @@ var _ = require('lodash');
 var axios = require('axios');
 var env = require('dotenv').config();
 
-let apiKey= `${process.env.GIF_AUTH}`;
-let randomEndPoint = 'https://api.giphy.com/v1/gifs/random';
-
-let url = `${randomEndPoint}?api_key=${apiKey}`;
-
-axios.get(url)
-  .then(function (response) {
-
-    console.log(response.data.data.images.original.url);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
-
 router.get('/', (req, res, next) => {
+  let apiKey= `${process.env.GIF_AUTH}`;
+  let randomEndPoint = 'https://api.giphy.com/v1/gifs/random';
+  let url = `${randomEndPoint}?api_key=${apiKey}`;
 
+  axios.get(url)
+    .then((response) => {
+      console.log(response.data.data.images.original.url);
+      res.send(response.data.data.images.original.url);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(error);
+    });
 });
 
 
