928723ebf1d17f84d13d1d01fc9b01c0af486ce4
Joseph Thompson <josephthompson@Josephs-MacBook-Pro.local>
2018-06-29 09:02:04 -0400

    search-request-to-giphy-api: added the cors module

--- routes/gifGetter.js
+++ routes/gifGetter.js
@@ -1,13 +1,29 @@
-var express = require('express');
-var router = express.Router();
-var _ = require('lodash');
-var axios = require('axios');
-var env = require('dotenv').config();
+let express = require('express');
+let router = express.Router();
+let _ = require('lodash');
+let axios = require('axios');
+let env = require('dotenv').config();
 
-router.get('/', (req, res, next) => {
-  let apiKey= `${process.env.GIF_AUTH}`;
+const apiKey= `${process.env.GIF_AUTH}`;
+
+router.get('/:search', (req, res, next) => {
   let randomEndPoint = 'https://api.giphy.com/v1/gifs/random';
   let url = `${randomEndPoint}?api_key=${apiKey}`;
+
+  axios.get(url)
+    .then((response) => {
+      console.log(response.data.data.images.original.url);
+      res.send(response.data.data.images.original.url);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send(error);
+    });
+});
+
+router.get('/search', (req, res, next) => {
+  let searchEndPoint = 'https://api.giphy.com/v1/gifs/search';
+  let url = `${searchEndPoint}?api_key=${apiKey}`;
 
   axios.get(url)
     .then((response) => {
